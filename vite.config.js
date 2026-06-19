4689d81179679af0b35ab63107007b2ea55cafab
Aleksa Jukic <aleksa.jukic.web@gmail.com>
2026-06-19 20:29:34 +0200

    Fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MyPortfolio/',
+  base: process.env.VERCEL ? '/' : '/MyPortfolio/',
   plugins: [vue(), vueDevTools()],
   css: {
     preprocessorOptions: {
